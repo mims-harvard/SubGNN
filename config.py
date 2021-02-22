09a726e2d206d0fc71c1cbca9abe2ff2a15502a8
Yang Liu <thomasliuy@gmail.com>
2021-02-22 17:53:42 -0500

    config PROJECT_ROOT

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
-
 from pathlib import Path
 
 # directory where data and results will be stored
 # TODO: *UPDATE TO YOUR DIRECTORY OF CHOICE*
-PROJECT_ROOT = Path('/mnt/subgraphs/data_to_release')
+PROJECT_ROOT = Path("/raid/home/public/YangLiu/SubGNN_data")
 
 # padding
 PAD_VALUE = 0
-
